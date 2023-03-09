603ad26232ff3ae5f49c2ecb90b69394f3b8d8ad
yuny903 <yuny903@gmail.com>
2023-03-09 20:25:21 +0900

    initial version of main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+  for i in range(0,lens(nums)):
+	  for j in range(i+1,lens(nums)):
+		  if nums[i]+nums[j]==target:
+			  return [i,j];
+		
+  return [-1,-1];
